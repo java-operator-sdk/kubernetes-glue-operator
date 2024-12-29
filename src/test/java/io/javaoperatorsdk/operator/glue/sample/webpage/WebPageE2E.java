1736095b72f425af04a3e0d9bb9e6c8e6a03891c
Attila Mészáros <a_meszaros@apple.com>
2024-12-29 19:32:43 +0100

    docs & test fix
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

--- src/test/java/io/javaoperatorsdk/operator/glue/sample/webpage/WebPageE2E.java
+++ src/test/java/io/javaoperatorsdk/operator/glue/sample/webpage/WebPageE2E.java
@@ -59,10 +59,12 @@ public class WebPageE2E {
       assertThat(ingress).isNotNull();
     });
 
+    var wp = client.resources(WebPage.class).withName("webpage1").get();
+    assertThat(wp.getStatus().getObservedGeneration()).isNotNull();
+
     client.resource(createdWebPage).delete();
 
     await().timeout(TestUtils.GC_WAIT_TIMEOUT).untilAsserted(() -> {
-      var wp = client.resources(WebPage.class).withName("webpage1").get();
       var deployment =
           client.resources(Deployment.class).withName(webPage.getMetadata().getName()).get();
       var configMap =
@@ -74,7 +76,7 @@ public class WebPageE2E {
       assertThat(configMap).isNull();
       assertThat(service).isNull();
       assertThat(ingress).isNull();
-      assertThat(wp.getStatus().getObservedGeneration()).isNotNull();
+
     });
   }
 
